d32946a917480bad0f9ffd4c3c84543ada5bcc52
Azhari Orayama <azhariorayama94@gmail.com>
2024-06-07 16:44:54 +0700

    tambahkan comment

--- routes.js
+++ routes.js
@@ -3,21 +3,27 @@
 module.exports = function(app) {
     var jsonku = require('./controller');
 
+    // ROUTE INDEX
     app.route('/')
         .get(jsonku.index);
 
+    // ROUTE TAMPILKAN DATA
     app.route('/tampil')
         .get(jsonku.tampilsemuamahasiswa);
 
+    // ROUTE TAMPILKAN BEDASARKAN ID
     app.route('/tampil/:id')
         .get(jsonku.tampilberdasarkanid);
 
+    // ROUTE TAMBAH DATA
     app.route('/tambah')
         .post(jsonku.tambahmahasiswa);
 
+    // ROUTE UBAH DATA BEDASARKAN ID
     app.route('/ubahdata')
         .put(jsonku.ubahDataMahasiswa);
 
+    // ROUTE HAPUS DATA
     app.route('/hapus')
         .delete(jsonku.hapusDataMahasiswa);
 }

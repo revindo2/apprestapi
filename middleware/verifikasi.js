b9da0e9f2fec58a21d430d79f79cf62e8f89f378
Azhari Orayama <azhariorayama94@gmail.com>
2024-06-10 14:11:22 +0700

    add lagi

--- middleware/verifikasi.js
+++ middleware/verifikasi.js
@@ -1,8 +1,9 @@
 const jwt = require('jsonwebtoken');
 const config = require('../config/secret');
 
-function verifikasi(roles){
+function verifikasi(){
     return function(req, rest, next){
+        var role = req.body.role;
         // cek authorize header
         var tokenWithBearer = req.headers.authorization;
         if (tokenWithBearer) {
@@ -12,7 +13,7 @@ function verifikasi(roles){
                 if (error) {
                     return rest.status(401).send({auth:false, message:'Token Tidak Terdaftar'});
                 }else{
-                    if (roles == 2) {
+                    if (role == 2) {
                         req.auth = decoded;
                         next();
                     }else{

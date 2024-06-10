12e3391e7ffa6188f2898e60c134f73c544c8193
Azhari Orayama <azhariorayama94@gmail.com>
2024-06-10 10:26:21 +0700

    add register user

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+var morgan = require('morgan');
 
 // parse aplication
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan('dev'));
 
 // panggil routes
 var routes = require('./routes');
 routes(app);
 
+// daftarkan menu routes dari index
+// const { use } = require('./middleware');
+app.use('/auth', require('./middleware'));
+
 app.listen(3000, () => {
-    console.log('Server started on port');
+    console.log('Server started on port 3000');
 });
